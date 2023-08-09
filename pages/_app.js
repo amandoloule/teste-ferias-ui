5484557418170d4320b55ba35edd70f05e90fb79
Amando Lourenco <amandoloule@gmail.com>
2023-08-09 01:31:26 -0300

    Changed

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,16 @@
 import { ApolloProvider } from '@apollo/client'
 import { useApollo } from '../lib/apollo'
 import { ChakraProvider } from '@chakra-ui/react'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   const apolloClient = useApollo(pageProps.initialApolloState || {})
 
   return (
     <ApolloProvider client={apolloClient}>
+      <Head>
+        <title>Teste Férias</title>
+      </Head>
       <ChakraProvider>
         <Component {...pageProps} />
       </ChakraProvider>
