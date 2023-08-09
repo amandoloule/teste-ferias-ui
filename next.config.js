c531ce048c55ab051e94f35f7a30fe8d52f74141
Amando Lourenco <amandoloule@gmail.com>
2023-08-09 02:43:43 -0300

    Changed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    API_ENDPOINT: 'https://active-growth-4a80177b9b.strapiapp.com/graphql',
+  },
 }
 
 module.exports = nextConfig
